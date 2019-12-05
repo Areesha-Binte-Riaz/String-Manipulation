237b0e2aae191d7f6f866fcf53f993b4069b4227
Areesha-Binte-Riaz <56113066+Areesha-Binte-Riaz@users.noreply.github.com>
2019-12-05 16:26:04 +0500

    Add files via upload

---
+++ Inverse a STR.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim STR, MyWord As String
+        Dim nextChar As Char
+        Dim count As Integer
+        count = 0
+        nextChar = " "
+        MyWord = " "
+        STR = " "
+        Console.WriteLine("Enter your String: ")
+        STR = Console.ReadLine
+        For count = 1 To Len(STR)
+            nextChar = Mid(STR, count, 1)
+            MyWord = nextChar & MyWord
+        Next
+        Console.WriteLine(MyWord)
+        Console.ReadKey()
+    End Sub
+
+End Module
