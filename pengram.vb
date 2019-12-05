77bbe0dc4ae5a9df34d023ac1e85d66b5942d637
Areesha-Binte-Riaz <56113066+Areesha-Binte-Riaz@users.noreply.github.com>
2019-12-05 11:47:49 +0500

    Add files via upload
    source code to check if sentence entered is a pangram

---
+++ pengram.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim STR1 As String
+        Dim Counter As Integer
+        Dim A As Integer
+        Dim Found As Boolean
+        Found = False
+        STR1 = " "
+        Counter = 0
+        A = 0
+        Console.Write("Enter String: ")
+        STR1 = Console.ReadLine
+        STR1 = UCase(STR1)
+        For Counter = 0 To 25
+            If InStr(STR1, Chr(65 + Counter)) > 0 Then
+                Found = True
+            Else
+                Found = False
+                Console.WriteLine("Sentence entered is not a pangram.")
+            End If
+        Next Counter
+        Console.WriteLine("Sentence entered is a pangram.")
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
