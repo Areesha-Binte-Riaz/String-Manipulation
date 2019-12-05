c554e92bbfb9b5815ed3d84b0e5ec7bef70f4790
Areesha-Binte-Riaz <56113066+Areesha-Binte-Riaz@users.noreply.github.com>
2019-12-05 18:31:25 +0500

    Add files via upload

---
+++ counting no. of alpha and digits.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim STR As String
+        Dim AlphaNum, DIgitNum, ASCII As Integer
+        Dim nextChar As Char
+        nextChar = " "
+        STR = " "
+        AlphaNum = 0
+        ASCII = 0
+        DIgitNum = 0
+        Console.WriteLine("Enter String : ")
+        STR = Console.ReadLine
+        STR = UCase(STR)
+        For count = 1 To Len(STR)
+            nextChar = Mid(STR, count, 1)
+            ASCII = Asc(nextChar)
+            If ASCII >= 65 And ASCII <= 90 Then
+                AlphaNum = AlphaNum + 1
+            Else
+                If ASCII >= 48 And ASCII <= 57 Then
+                    DIgitNum = DIgitNum + 1
+                Else
+                    Console.WriteLine("The character entered contains more than just digits and alpabets : " & nextChar)
+                End If
+            End If
+        Next
+        Console.WriteLine("The number of alphabets entered : " & AlphaNum)
+        Console.WriteLine("The number of digits entered : " & DIgitNum)
+        Console.ReadKey()
+    End Sub
+
+End Module
