27596b1438c0af29a9eb0f3a3c85dd5c0f78cfe1
Areesha-Binte-Riaz <56113066+Areesha-Binte-Riaz@users.noreply.github.com>
2019-12-05 15:57:32 +0500

    Add files via upload

---
+++ STR after truncating detected char.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2 As String
+        Dim rChar, nextChar As Char
+        Dim count As Integer
+        Str1 = " "
+        Str2 = " "
+        rChar = " "
+        nextChar = " "
+        count = 0
+        Console.WriteLine("Enter String:")
+        Str1 = Console.ReadLine
+        Console.WriteLine("Enter character to remove:")
+        rChar = Console.ReadLine
+        For count = 1 To Len(Str1)
+            nextChar = Mid(Str1, count, 1)
+            If nextChar <> rChar Then
+                Str2 = Str2 & nextChar
+            End If
+        Next
+        Console.WriteLine("Final String: " & Str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
