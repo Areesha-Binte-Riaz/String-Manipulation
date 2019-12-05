38aed53b2d273ff0e1ce3783cf40121a1ddedf05
Areesha-Binte-Riaz <56113066+Areesha-Binte-Riaz@users.noreply.github.com>
2019-12-05 16:18:45 +0500

    Add files via upload

---
+++ Isolated words of a STR.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim String1, MyWord As String
+        Dim count As Integer
+        Dim nextChar As Char
+        String1 = " "
+        MyWord = " "
+        count = 0
+        nextChar = " "
+        Console.WriteLine("Enter Your String : ")
+        String1 = Console.ReadLine
+        For count = 1 To Len(String1)
+            nextChar = Mid(String1, count, 1)
+            If nextChar = " " Then
+                Console.WriteLine(MyWord)
+                MyWord = " "
+            Else
+                MyWord = MyWord & nextChar
+            End If
+        Next
+        Console.WriteLine(MyWord)
+        Console.ReadKey()
+    End Sub
+
+End Module
