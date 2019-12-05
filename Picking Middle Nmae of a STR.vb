6e78301fb1dcdefac81fdb76448a9ad095e16ade
Areesha-Binte-Riaz <56113066+Areesha-Binte-Riaz@users.noreply.github.com>
2019-12-05 16:07:48 +0500

    Add files via upload

---
+++ Picking Middle Nmae of a STR.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NameX, MN As String
+        Dim FPos, SPos As Integer
+        NameX = " "
+        MN = " "
+        FPos = 0
+        SPos = 0
+        Console.WriteLine("Input Name : ")
+        NameX = Console.ReadLine
+        FPos = InStr(NameX, " ")
+        SPos = InStr(FPos + 1, NameX, " ")
+        MN = Mid(NameX, FPos + 1, SPos - FPos - 1)
+        Console.WriteLine("Middle Name: " & MN)
+        Console.ReadKey()
+    End Sub
+
+End Module
